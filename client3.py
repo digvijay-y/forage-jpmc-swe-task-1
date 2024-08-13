5875d92dc1c9f9ddaa6a13af6c286f18ac9f8da6
digvijay-y <yewaredigvijay@gmail.com>
2024-08-13 22:50:19 +0530

    Test1 + bounes

--- client3.py
+++ client3.py
@@ -46,18 +46,17 @@ def getRatio(price_a, price_b):
         return
     return price_a/price_b # returns Ratio of prices
 
-
-# Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
-    for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
-        prices={} # stores the key value for stock
-        for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            prices[stock] = price #adding stock and its price to prices
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+	# Query the price once every N seconds.
+	for _ in range(N):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        print("Ratio %s" % getRatio(prices("ABC"), prices("ABC")))
+		""" ----------- Update to get the ratio --------------- """
+		prices = {}
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			prices[stock] = price
+			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+		print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
